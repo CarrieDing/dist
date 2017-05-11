c3a7ed1b9997953c0196f9f2ab25ad5103525d26
CarrieDing <jiakl@Hotmail.com>
2017-05-11 10:36:00 +0800

    Update vendor.d41d8cd9.js

---
+++ scripts/vendor.d41d8cd9.js
@@ -0,0 +1,38 @@
+<script src="bower_components/jquery/dist/jquery.js"></script>
+    <script src="bower_components/angular/angular.js"></script>
+    <script src="bower_components/bootstrap/dist/js/bootstrap.js"></script>
+    <script src="bower_components/angular-animate/angular-animate.js"></script>
+    <script src="bower_components/angular-cookies/angular-cookies.js"></script>
+    <script src="bower_components/angular-resource/angular-resource.js"></script>
+    <script src="bower_components/angular-route/angular-route.js"></script>
+    <script src="bower_components/angular-sanitize/angular-sanitize.js"></script>
+    <script src="bower_components/angular-touch/angular-touch.js"></script>
+    <script src="bower_components/angular-strap/dist/angular-strap.js"></script>
+    <script src="bower_components/angular-strap/dist/angular-strap.tpl.js"></script>
+    <script src="bower_components/lodash/lodash.js"></script>
+    <!-- endbower -->
+    <!-- endbuild -->
+
+        <!-- build:js({.tmp,app}) scripts/scripts.js -->
+        <script src="scripts/app.js"></script>
+        <script src="scripts/directives/tsk6-article-panel.js"></script>
+        <script src="scripts/controllers/articlelist.js"></script>
+        <script src="scripts/controllers/addarticle.js"></script>
+        <script src="scripts/controllers/welcomepage.js"></script>
+        <script src="scripts/directives/tsk6-article-table.js"></script>
+        <script src="scripts/directives/tsk6-article-row.js"></script>
+        <script src="scripts/services/article-service.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.2/angular.js"></script>
+    <link rel="stylesheet"; href="https://unpkg.com/ng-table@2.0.2/bundles/ng-table.min.css">
+    <script src="https://unpkg.com/ng-table@2.0.2/bundles/ng-table.min.js"></script>
+
+        <script src="scripts/controllers/login.js"></script>
+
+        <script src="scripts/directives/file-model.js"></script>
+        <script src="scripts/services/file-reader.js"></script>
+        <script src="scripts/services/articletable-service.js"></script>
+        <script src="scripts/controllers/dashboard.js"></script>
+        <script src="scripts/controllers/main.js"></script>
+        <script src="scripts/directives/tsk6-dashboard-panel.js"></script>
+        <script src="scripts/services/user-service.js"></script>
+        <!-- endbuild -->
